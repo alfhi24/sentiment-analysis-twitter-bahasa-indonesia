58b51d90fa9330e99711c85d92245ebcbd650990
Muhammad Alfhi Saputra <alfhisa@student.telkomuniversity.ac.id>
2019-10-01 12:31:49 +0700

    Add files via upload

---
+++ sentiment_twitter.py
@@ -0,0 +1,28 @@
+import tweepy
+from textblob import TextBlob
+from tweepy import OAuthHandler
+import twitter_auth # CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+
+auth = OAuthHandler(twitter_auth.CONSUMER_KEY, twitter_auth.CONSUMER_SECRET)
+auth.set_access_token(twitter_auth.ACCESS_TOKEN, twitter_auth.ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+jml = 20
+tweets = api.search(q=['mahasiswa', 'DPR'], count = jml)
+
+sum_polarity = 0
+for tweet in tweets : 
+    print(tweet.text)
+    analysis = TextBlob(tweet.text)
+    hasil = analysis.translate(from_lang = 'id', to = 'en')
+    print(hasil.sentiment, "/n")
+    sum_polarity += hasil.polarity
+
+value = sum_polarity / jml
+
+if (value > 0) :
+    print(value, " ", "Positive")
+elif (value == 0) : 
+    print(value, " ", "Netral")
+else  :
+    print(value, " ", "Negative")
